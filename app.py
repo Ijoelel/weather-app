2d009bdeadf8adf3789e38edd68d55270a314afb
Ijoelel <fianarnatha13@gmail.com>
2024-10-31 13:24:03 +0700

    update layer.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ customtkinter.set_default_color_theme("blue")
 class App(customtkinter.CTk):
 
     APP_NAME = "WeatherMaps"
-    WIDTH = 1366
-    HEIGHT = 736
+    WIDTH = 1000
+    HEIGHT = 550
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,6 +23,7 @@ class App(customtkinter.CTk):
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.bind("<Command-q>", self.on_closing)
         self.bind("<Command-w>", self.on_closing)
+        self.bind("<Configure>", self.resize)
         self.createcommand('tk::mac::Quit', self.on_closing)
 
         self.marker_list = []
@@ -46,19 +47,21 @@ class App(customtkinter.CTk):
         self.map_widget = TkinterMapView(self.frame, corner_radius=0)
         self.map_widget.grid(row=1, rowspan=1, column=0, columnspan=3, sticky="nsew", padx=(0, 0), pady=(0, 0))
 
-        options = ["Cloud", "Precipitation", "Pressure", "Wind", "Temperature"]
-        self.layer_selector = customtkinter.CTkOptionMenu(master=self.map_widget, values=options, command=self.change_layer)
-        self.layer_selector.grid(row=0, column=0, padx=10, pady=10)
-
         # Entry input dan tombol search
         self.search_button = customtkinter.CTkButton(master=self.map_widget, width=90, height=30, text="Search", command=self.search_event)
         self.search_button.grid(row=0, column=0, padx=5, pady=5)
-        self.search_button.place(x=App.WIDTH - self.search_button._current_width - 20, y=20)
+        self.search_button.place(x=self.winfo_width() - self.search_button._current_width - 20, y=20)
         
         self.entry = customtkinter.CTkEntry(master=self.map_widget, width=400, height=30, placeholder_text="Type address")
         self.entry.grid(padx=(5, 10), pady=5)
         self.entry.place(x= App.WIDTH - self.entry._current_width - self.search_button._current_width - 30, y=20)
         self.entry.bind("<Return>", self.search_event)
+
+        # Layer Selector
+        options = ["Cloud", "Precipitation", "Pressure", "Wind", "Temperature"]
+        self.layer_selector = customtkinter.CTkOptionMenu(master=self.map_widget, values=options, command=self.change_layer)
+        self.layer_selector.grid(row=0, column=0, padx=10, pady=10)
+        self.layer_selector.place(x=App.WIDTH - self.entry._current_width - self.layer_selector._current_width - self.search_button._current_width - 50, y=20)
 
         # Set default values
         self.map_widget.set_position(-7.2461420, 112.7367966)  # Surabaya
@@ -69,9 +72,10 @@ class App(customtkinter.CTk):
         # Set default layer (e.g., CloudLayer)
         self.active_layer = CloudLayer(self.map_widget)
 
-    def display_weather_data(self, data):
-        # Show the data in a label or pop-up as desired
-        print(data)  # Or replace with code to display in the app
+    def resize(self, event):
+        self.search_button.place(x=self.winfo_width() - self.search_button._current_width - 20, y=20)
+        self.entry.place(x= self.winfo_width() - self.entry._current_width - self.search_button._current_width - 30, y=20)
+        self.layer_selector.place(x=self.winfo_width() - self.entry._current_width - self.layer_selector._current_width - self.search_button._current_width - 50, y=20)
 
     def change_layer(self, selection):
         layer_map = {
@@ -87,24 +91,24 @@ class App(customtkinter.CTk):
     def search_event(self, event=None):
         location_query = self.entry.get()
         if location_query:
-            url = f"https://nominatim.openstreetmap.org/search?q={location_query}&format=jsonv2&addressdetails=1&limit=1"
-            try:
-                response = requests.get(url)
-                data = response.json()
+            url = f"https://api-v2.distancematrix.ai/maps/api/geocode/json?address={location_query}&key=89rzA8N4hHGOm25oStS6aRKDLKuOEex9wekgHZyBgkbXKzKP5FMisNVj8bu5MiTs"
+            print(url)
+            response = requests.get(url)
+            print(response.json()['result'][0]['geometry']['location'])
+            data = response.json()['result'][0]['geometry']['location']
 
-                if data:
-                    # Ambil latitude dan longitude dari hasil
-                    latitude = float(data[0]["lat"])
-                    longitude = float(data[0]["lon"])
+            if data:
+                # Ambil latitude dan longitude dari hasil
+                latitude = float(data["lat"])
+                longitude = float(data["lng"])
 
-                    # Atur posisi pada peta
-                    self.map_widget.set_position(latitude, longitude)
+                # Atur posisi pada peta
+                self.map_widget.set_position(latitude, longitude)
+                self.map_widget.set_zoom(17)
 
-                    print(f"Location found: {data[0]['display_name']} at {latitude}, {longitude}")
-                else:
-                    print("Location not found.")
-            except Exception as e:
-                print(f"Error: {e}")
+                print(f"Location found: at {latitude}, {longitude}")
+            else:
+                print("Location not found.")
 
     def on_closing(self, event=0):
         self.destroy()
--- layer.py
+++ layer.py
@@ -1,44 +1,128 @@
 from typing import TYPE_CHECKING
+import customtkinter
+from PIL import Image
+import requests
 
 if TYPE_CHECKING:
     from tkintermapview.map_widget import TkinterMapView
-
-class Layer:
+    
+class Layer():
     def __init__(self, map_widget: "TkinterMapView"):
         self.map_widget = map_widget
         self.map_widget.canvas.bind("<Double-1>", self.on_map_double_click)
+
+        self.frame = customtkinter.CTkFrame(master=self.map_widget, fg_color="white")
+        self.frame.columnconfigure(0, weight=1, minsize=100)
+        self.frame.columnconfigure(1, weight=1)
 
     def set_overlay(self, overlay_url_template):
         self.map_widget.set_tile_server("https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png")
         self.map_widget.set_overlay_tile_server(overlay_url_template)
 
     def on_map_double_click(self, event):
+        pass
+
+    def get_data(self, layer, event):
         lat, lon = self.map_widget.convert_canvas_coords_to_decimal_coords(event.x, event.y)
 
-    def get_data(self, lat, lon):
-        pass  # Will be overridden by subclasses to fetch layer-specific data
+    def close_frame(self):
+        self.frame.place_forget()
+
+    def clear_all_inside_frame(self):
+        # Iterate through every widget inside the frame
+        for widget in self.frame.winfo_children():
+            widget.destroy()
 
 class CloudLayer(Layer):
     def __init__(self, map_widget):
         super().__init__(map_widget)
         self.set_overlay("https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=bdb5a3638f7c947577e361feb6a14471")
 
-    def get_data(self, lat, lon):
-        pass
-
     def on_map_double_click(self, event):
+        self.clear_all_inside_frame()
+
+        # Close Button
+        cloud_icon = customtkinter.CTkImage(light_image=Image.open("x.png"),
+                                  size=(16, 16))
+
+        self.frame_close_button = customtkinter.CTkButton(master=self.frame, width=20, height=20, text="", image=cloud_icon, fg_color="transparent", hover=False, command=self.close_frame)
+        self.frame_close_button.grid(row=0, column=2, padx=5, pady=(4, 0))
+        
+        # Main Label "Details"
+        self.frame_label = customtkinter.CTkLabel(master=self.frame, text="Details", text_color='black')
+        self.frame_label.grid(row=0, columnspan=2)
+
+        # Get latitude and longitude from map
         lat, lon = self.map_widget.convert_canvas_coords_to_decimal_coords(event.x, event.y)
-        # data = self.active_layer.get_data(lat, lon)
-        # if data:
-        #     self.display_weather_data(data)
+        weather_url = f"https://api.tomorrow.io/v4/weather/realtime?location={lat}, {lon}&apikey=AqwaRL5CVxt61JpyfxrCHeZAto4W6zlH"
+        data_url = f"https://api.geoapify.com/v1/geocode/reverse?lat={lat}&lon={lon}&apiKey=0d010075997245ca8559e806edf4a67c"
+        headers = {"accept": "application/json"}
+
+        # Call API
+        cloud_response = (requests.get(weather_url, headers=headers)).json()["data"]["values"]
+        data_response = (requests.get(data_url, headers=headers)).json()['features'][0]['properties']
+
+        # print(cloud_response)
+        print(data_response.get('country', 'hehe'))
+
+        if data_response.get('country'):
+            country_label = customtkinter.CTkLabel(master=self.frame, text="country", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+            country_label.grid(row=1, column=0)
+            state_label = customtkinter.CTkLabel(master=self.frame, text="state", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+            state_label.grid(row=2, column=0)
+
+            country_data = customtkinter.CTkLabel(master=self.frame, text=data_response["country"] if data_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+            country_data.grid(row=1, column=1)
+            state_label = customtkinter.CTkLabel(master=self.frame, text=data_response['state'] if data_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+            state_label.grid(row=2, column=1)
+        elif data_response.get('ocean'):
+            ocean_name_label = customtkinter.CTkLabel(master=self.frame, text="name", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+            ocean_name_label.grid(row=1, column=0)
+            ocean_label = customtkinter.CTkLabel(master=self.frame, text="ocean", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+            ocean_label.grid(row=2, column=0)
+
+            country_data = customtkinter.CTkLabel(master=self.frame, text=data_response["name"] if data_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+            country_data.grid(row=1, column=1)
+            state_label = customtkinter.CTkLabel(master=self.frame, text=data_response['ocean'] if data_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+            state_label.grid(row=2, column=1)
+        elif data_response.get('marinearea'):
+            marinearea_name_label = customtkinter.CTkLabel(master=self.frame, text="name", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+            marinearea_name_label.grid(row=1, column=0)
+            marinearea_label = customtkinter.CTkLabel(master=self.frame, text="sea", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+            marinearea_label.grid(row=2, column=0)
+
+            country_data = customtkinter.CTkLabel(master=self.frame, text=data_response["name"] if data_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+            country_data.grid(row=1, column=1)
+            state_label = customtkinter.CTkLabel(master=self.frame, text=data_response['marinearea'] if data_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+            state_label.grid(row=2, column=1)
+        
+        cloud_cover_label = customtkinter.CTkLabel(master=self.frame, text="cloud_cover", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+        cloud_cover_label.grid(row=3, column=0)
+        cloud_base_label = customtkinter.CTkLabel(master=self.frame, text="cloud_base", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+        cloud_base_label.grid(row=4, column=0)
+        cloud_ceiling_label = customtkinter.CTkLabel(master=self.frame, text="cloud_ceiling", text_color="#888", anchor='e', justify='right',width=100, padx=8)
+        cloud_ceiling_label.grid(row=5, column=0)
+
+        cloud_cover_label = customtkinter.CTkLabel(master=self.frame, text=f"{cloud_response['cloudCover']}%" if cloud_response else 'null', text_color="black", anchor='w', justify='left',width=120, padx=8)
+        cloud_cover_label.grid(row=3, column=1)
+        cloud_base_label = customtkinter.CTkLabel(master=self.frame, text=f"{cloud_response['cloudBase']} km", text_color="black", anchor='w', justify='left',width=120, padx=8)
+        cloud_base_label.grid(row=4, column=1)
+        cloud_ceiling_label = customtkinter.CTkLabel(master=self.frame, text=f"{cloud_response['cloudCeiling']} km" if cloud_response['cloudCeiling'] != None else "null", text_color="black", anchor='w', justify='left',width=120, padx=8)
+        cloud_ceiling_label.grid(row=5, column=1)
+
+        if self.map_widget.winfo_width() - event.x < self.frame._current_width and self.map_widget.winfo_height() - event.y < self.frame._current_height:
+            self.frame.place(x=event.x - self.frame._current_width, y=event.y - self.frame._current_height)
+        elif self.map_widget.winfo_width() - event.x < self.frame._current_width:
+            self.frame.place(x=event.x - self.frame._current_width, y=event.y)
+        elif self.map_widget.winfo_height() - event.y < self.frame._current_height:
+            self.frame.place(x=event.x, y=event.y - self.frame._current_height)
+        else:
+            self.frame.place(x=event.x, y=event.y)
 
 class PrecipitationLayer(Layer):
     def __init__(self, map_widget):
         super().__init__(map_widget)
         self.set_overlay("https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=bdb5a3638f7c947577e361feb6a14471")
-
-    def get_data(self, lat, lon):
-        pass
 
     def on_map_double_click(self, event):
         lat, lon = self.map_widget.convert_canvas_coords_to_decimal_coords(event.x, event.y)
@@ -48,9 +132,6 @@ class PressureLayer(Layer):
         super().__init__(map_widget)
         self.set_overlay("https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=bdb5a3638f7c947577e361feb6a14471")
 
-    def get_data(self, lat, lon):
-        pass
-
     def on_map_double_click(self, event):
         lat, lon = self.map_widget.convert_canvas_coords_to_decimal_coords(event.x, event.y)
 
@@ -58,9 +139,6 @@ class WindLayer(Layer):
     def __init__(self, map_widget):
         super().__init__(map_widget)
         self.set_overlay("https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=bdb5a3638f7c947577e361feb6a14471")
-
-    def get_data(self, lat, lon):
-        pass
 
     def on_map_double_click(self, event):
         lat, lon = self.map_widget.convert_canvas_coords_to_decimal_coords(event.x, event.y)
